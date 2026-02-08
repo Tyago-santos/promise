0fcd35927490c04dabbcd4363b97c07e4411681c
tiago santos <xlzthyago@gmail.com>
2026-02-08 11:14:29 -0300

    fix: fix rotes provite

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,35 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
+import { Outlet, createRootRoute, useLocation } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { HeadManager } from "@/components/HeadManager";
+import Header from "../components/Header";
+import { useEffect, useState } from "react";
 export const Route = createRootRoute({
-  component: () => (
+  component: App,
+});
+
+export default function App() {
+  const [headerShow, setHeaderShow] = useState(true);
+  const location = useLocation();
+  const hiddenRoutes = ["/perfil", "/register"];
+
+  useEffect(() => {
+    const vefifyRoute = () => {
+      console.log(location.pathname);
+      if (hiddenRoutes.includes(location.pathname)) setHeaderShow(false);
+    };
+
+    vefifyRoute();
+  });
+
+  return (
     <>
+      <HeadManager />
+
+      {headerShow && <Header />}
       <Outlet />
-      <TanStackDevtools
+
+      {/* <TanStackDevtools
         config={{
           position: 'bottom-right',
         }}
@@ -16,7 +39,7 @@ export const Route = createRootRoute({
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
-      />
+      /> */}
     </>
-  ),
-})
+  );
+}
