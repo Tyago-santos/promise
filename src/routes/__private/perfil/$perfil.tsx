1ae2adb661b82c3d3b8d3e8cb78d4e9285a608ce
tiago santos <xlzthyago@gmail.com>
2026-02-09 14:13:38 -0300

    chore: transform perfil for dinamic

--- src/routes/__private/perfil/$perfil.tsx
+++ src/routes/__private/perfil/$perfil.tsx
@@ -3,7 +3,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import HeaderPefil from "@/components/HeaderPerfil";
 import InforPerfil from "@/components/InforPerfil";
 
-export const Route = createFileRoute("/__private/perfil")({
+export const Route = createFileRoute("/__private/$perfil")({
   component: RouteComponent,
 });
 
