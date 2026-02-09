989c934d668e49b23864fda3a9a6e96d5f1752d8
tiago santos <xlzthyago@gmail.com>
2026-02-09 14:14:43 -0300

    feat: adding component contact mobile

---
+++ src/components/ContactMobile.tsx
@@ -0,0 +1,12 @@
+import HeaderPefil from "./HeaderPerfil";
+
+
+
+export default function ContactMobile() {
+  return (
+    <main>
+      <HeaderPefil />
+      <div className="h-screen bg-green-400"></div>
+    </main>
+  );
+}
