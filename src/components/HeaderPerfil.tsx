9c88e11e8c3ae162dd25dd41805dad1ae6f6ad06
tiago santos <xlzthyago@gmail.com>
2026-02-10 09:00:21 -0300

    styles: change color text of name and icon

--- src/components/HeaderPerfil.tsx
+++ src/components/HeaderPerfil.tsx
@@ -1,15 +1,25 @@
+import { useRouter } from "@tanstack/react-router";
 import { FaArrowLeft } from "react-icons/fa";
 
-export default function HeaderPefil() {
+type PropsType = {
+  name: string;
+};
+
+export default function HeaderPerfil({ name }: PropsType) {
+  const router = useRouter();
+
   return (
-    <header>
+    <header className="border border-gray-200">
       <div className="flex gap-2  items-center p-4">
-        <div className="rounded-full border-2 border-text p-2">
+        <div
+          onClick={() => router.history.back()}
+          className="rounded-full border-2 border-gray-500 text-gray-500  p-2"
+        >
           <FaArrowLeft />
         </div>
         <div>
-          <h3 className="font-bold font-display">Tiago Santos</h3>
-          <span className="text-sm text-text font-sans">10 posts</span>
+          <h3 className="font-bold font-display text-text">{name}</h3>
+          <span className="text-sm text-text font-sans"></span>
         </div>
       </div>
     </header>
