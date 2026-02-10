50364ba63641a4e0e465f52da1e744a00ccda738
tiago santos <xlzthyago@gmail.com>
2026-02-10 18:06:23 -0300

    upate

--- src/components/HeaderPerfil.tsx
+++ src/components/HeaderPerfil.tsx
@@ -3,22 +3,39 @@ import { FaArrowLeft } from "react-icons/fa";
 
 type PropsType = {
   name: string;
+  match: boolean;
 };
 
-export default function HeaderPerfil({ name }: PropsType) {
+export default function HeaderPerfil({ name, match }: PropsType) {
   const router = useRouter();
 
   return (
-    <header className="border border-gray-200">
-      <div className="flex gap-2  items-center p-4">
+    <header
+      className={
+        !match ? `border border-gray-200 p-4` : `border border-transparent`
+      }
+    >
+      <div className="flex gap-2  items-center ">
         <div
           onClick={() => router.history.back()}
-          className="rounded-full border-2 border-gray-500 text-gray-500  p-2"
+          className={
+            !match
+              ? `rounded-full border-2 border-gray-500 text-gray-500  p-2`
+              : `rounded-full  text-white  p-2`
+          }
         >
           <FaArrowLeft />
         </div>
         <div>
-          <h3 className="font-bold font-display text-text">{name}</h3>
+          <h3
+            className={
+              !match
+                ? `font-bold font-display text-text `
+                : `font-bold font-display text-white`
+            }
+          >
+            {name}
+          </h3>
           <span className="text-sm text-text font-sans"></span>
         </div>
       </div>
