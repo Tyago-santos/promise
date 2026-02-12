e4682b6757951477d0d5de7fc4eb094de8b470bb
tiago santos <xlzthyago@gmail.com>
2026-02-12 17:42:24 -0300

    feat: create component moda search

--- src/components/ModalSearch.tsx
+++ src/components/ModalSearch.tsx
@@ -1,10 +1,15 @@
-import { posts } from "@/api";
+import { useModalSearchSore } from "@/store/useModalSeachStore";
 
 export default function ModalSearch() {
+  const posts = useModalSearchSore((state) => state.posts);
+
   return (
-    <div className="fixed hidden right-5 overflow-scroll  left-5 bg-white top-14 rounded-md bottom-20 z-10000">
+    <div className="fixed  right-5 overflow-scroll  left-5 bg-white top-14 rounded-md bottom-20 z-10000">
       {posts.map((perfil) => (
-        <button className=" flex w-full items-center border-b-text border gap-2 p-4">
+        <button
+          key={perfil.id}
+          className=" flex w-full items-center border-gray-200 border-b gap-2 p-4"
+        >
           <img
             className="size-14 rounded-full object-cover"
             src={perfil.imagens}
