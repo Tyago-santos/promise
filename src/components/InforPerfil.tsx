e5a05001bbfe2a8a3c26eb3453293734a63b7f15
tiago santos <xlzthyago@gmail.com>
2026-02-13 11:50:16 -0300

    chore: implement and configuration perfil personal

--- src/components/InforPerfil.tsx
+++ src/components/InforPerfil.tsx
@@ -1,19 +1,20 @@
-import type { PostType } from "@/api";
+import type { PostType, UserProfile } from "@/api";
 import PostContent from "./PostContent";
+import { LocateIcon } from "lucide-react";
 
 type PropsType = {
-  path: string;
   posts: PostType[];
+  inforPerfil: UserProfile;
 };
 
-export default function InforPerfil({ path, posts }: PropsType) {
+export default function InforPerfil({ posts, inforPerfil }: PropsType) {
   return (
     <div className="">
-      <div className="h-60 z-99">
+      <div className="h-65 z-99">
         <img className="max-h-full " src="/image_post2.jpg" alt="poster" />
       </div>
 
-      <div className=" bg-white transform -translate-y-30 border-b border-gray-200 pb-3    px-4">
+      <div className=" bg-white transform -translate-y-30 border-b pt-3 border-gray-200 pb-3    px-4">
         <div className="flex justify-between items-center">
           <div
             className="h-20 w-20 -mt-8 rounded-full 
@@ -21,39 +22,57 @@ export default function InforPerfil({ path, posts }: PropsType) {
           >
             <img
               className="max-w-full max-h-full    scale-[1.5] "
-              src="image_perfil.png"
+              src={inforPerfil.photos[0]}
               alt="imagem de perfil"
             />
           </div>
 
           <button
             className={`
-                  px-4 py-1.5 rounded-full font-medium text-sm transition-all
-                    bg-blue-500 hover:bg-blue-600 text-white shadow-md hover:shadow-lg
+                 hover:bg-gradient-to-r/80 text-white shadow-md hover:shadow-lg 
+                   bg-gradient-to-r from-pink-500 px-2 text-[10px] py-2 rounded-full to-purple-500
                 `}
           >
-            Editar perfil
+            Mandar Mensagem
           </button>
+          {/* <button
+            className={`
+                 hover:bg-gradient-to-r/80 text-text shadow-md hover:shadow-lg 
+                   bg-gradient-to-r from-primary to-secondary px-4 
+                   text-[10px] py-2 rounded-full 
+                `}
+          >
+            Mandar mensagem
+          </button> */}
         </div>
 
         <div>
           <div>
             <span className="font-bold my-2 text-xl block font-display">
-              Tiago Santos
+              {inforPerfil.name}
             </span>
-            <span className="font-smibod text-sm font-fans">25 anos</span>
+            <span className="font-semibold text-sm font-fans">
+              {inforPerfil.age} anos
+            </span>
           </div>
 
-          <p className="my-2">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            molestiae, neque, nihil commodi, ducimus laborum rerum deleniti
-            aliquid veniam quia at? Excepturi, quasi. Fugiat dolor ea explicabo
-            illum beatae delectus.
-          </p>
+          <p className="my-2">{inforPerfil.bio}</p>
+
+          <div className="flex items-center gap-2 text-text">
+            <LocateIcon size={20} />
+            {inforPerfil.location}
+          </div>
+
+          <div className="flex gap-2 flex-wrap font-semibold font-display my-3  ">
+            {inforPerfil.interests.length > 0 &&
+              inforPerfil.interests.map((inte) => (
+                <span key={inte}>#{inte}</span>
+              ))}
+          </div>
         </div>
       </div>
       <div className="transform -translate-y-30">
-        <PostContent path={path} posts={posts} />
+        <PostContent posts={posts} />
       </div>
     </div>
   );
