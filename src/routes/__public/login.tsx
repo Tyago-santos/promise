63bb19d327e182005e0d32321896b0b70640aed1
tiago santos <xlzthyago@gmail.com>
2026-02-20 12:12:24 -0300

    chore: implement validade data login

--- src/routes/__public/login.tsx
+++ src/routes/__public/login.tsx
@@ -25,16 +25,20 @@ type FormType = {
 };
 
 function App() {
-  const { register, handleSubmit } = useForm<FormType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormType>();
   const navigate = useNavigate();
   const [passwordIcon, setPasswordIcon] = useState(false);
 
   const handleSubmitForm = () => {
     alert("envou");
   };
-  const handleCreatePerfil = () => {
-    navigate({ to: "/create_person" });
-  };
+  // const handleCreatePerfil = () => {
+  //   navigate({ to: "/create_person" });
+  // };
 
   return (
     <main className="bg-background">
@@ -51,15 +55,27 @@ function App() {
             Faça Login
           </h4>
           <div
-            className="flex items-center justify-between  
-                block bg-gray-300 p-4 rounded-sm font-sans mt-3 w-full"
+            className={
+              errors.email
+                ? `block bg-gray-300 p-4 my-2 rounded-lg font-sans w-full 
+            focus:outline-none focus:ring-2 focus:ring-secondary/30 border border-red-500 flex`
+                : `block bg-gray-300 p-4 my-2 rounded-lg font-sans w-full 
+            focus:outline-none focus:ring-2 focus:ring-secondary/30 flex`
+            }
           >
             <input
-              {...register("password")}
+              {...register("password", {
+                required: "Camppo obrigatório*",
+                minLength: {
+                  value: 4,
+                  message: "senha tem ter no mínimo 4 caracteres",
+                },
+              })}
               placeholder="Digite sua senha"
               type={passwordIcon ? "password" : "text"}
               className="outline-none w-full block"
             />
+
             <div
               className="cursor-pointer"
               onClick={() => setPasswordIcon((prev) => !prev)}
@@ -71,13 +87,41 @@ function App() {
               )}
             </div>
           </div>
+
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+
           <input
-            {...register("email")}
+            {...register("email", {
+              required: "Campo obrigatório*",
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "este campo só aceita numeros",
+              },
+            })}
+            className={
+              errors.email
+                ? `block bg-gray-300 p-4 my-2 rounded-lg font-sans w-full 
+            focus:outline-none focus:ring-2 focus:ring-secondary/30 border border-red-500`
+                : `block bg-gray-300 p-4 my-2 rounded-lg font-sans w-full 
+            focus:outline-none focus:ring-2 focus:ring-secondary/30`
+            }
             placeholder="Digite seu email"
-            className="block bg-gray-300 block p-4 rounded-sm font-sans mt-3 w-full"
           />
+
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+
+          <Link
+            to="/forget"
+            className="text-text cursor-pointer hover:underline "
+          >
+            Esqueci minha senha
+          </Link>
+
           <input
-            onClick={handleCreatePerfil}
             className="bg-gradient-to-r from-primary to-secondary 
              hover:from-primary/90 hover:to-secondary/90
              active:scale-[0.98]
@@ -91,7 +135,10 @@ function App() {
             value="Fazer Login"
           />
           <div className="mt-3 cursor-pointer text-center">
-            <Link to="/register" className=" mr-2 text-sans text-text ">
+            <Link
+              to="/register"
+              className=" mr-2 text-sans text-text hover:underline "
+            >
               Não possui uma conta? Faça cadastro
             </Link>
           </div>
