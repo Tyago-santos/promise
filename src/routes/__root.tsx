5e1d7d0ab5a79a2c7172872bfc773c78c13a2c0b
tiago santos <xlzthyago@gmail.com>
2026-02-27 16:31:37 -0300

    chore: implement notFoundapage

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Outlet,
   // createRootRoute,
   createRootRouteWithContext,
@@ -8,6 +9,16 @@ import { HeadManager } from "@/components/HeadManager";
 import type { RouterContext } from "@/main";
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: App,
+
+  notFoundComponent: () => (
+    <div className="text-center p-10">
+      <h1>404 - Página não encontrada</h1>
+      <p>Desculpe, a página que você procura não existe.</p>
+      <Link to="/" className="text-blue-500">
+        Voltar para a Home
+      </Link>
+    </div>
+  ),
 });
 
 export default function App() {
